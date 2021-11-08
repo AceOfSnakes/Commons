c23b515c30ce1507aae29e43cb644055a018cd06
Ace Of Snakes <aceofsnakesmain@gmail.com>
2021-11-08 22:30:21 +0300

    Update commons.cpp

--- commons.cpp
+++ commons.cpp
@@ -53,6 +53,8 @@ QString Commons::compilerQString() {
     /*
      * https://docs.microsoft.com/en-us/cpp/preprocessor/predefined-macros
      */
+#elif _MSC_VER >= 1931
+    compiler.append(" 2022 / MSVC++ 17.1");
 #elif _MSC_VER >= 1930
     compiler.append(" 2022 / MSVC++ 17.0");
 #elif _MSC_VER >= 1929
